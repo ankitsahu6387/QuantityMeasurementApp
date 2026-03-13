a43c03aa414b78d09b705dbd4161a2cc860496d7
Ankit Sahu <ankits6387@gmail.com>
2026-03-13 16:59:14 +0530

    [Ankit Sahu] Add .added UC16

---
+++ src/main/java/com/Apps/quantitymeasurement/exception/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.Apps.quantitymeasurement.exception;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
